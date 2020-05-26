7a9154fed03545e68cddd67270bc0fc52e84a3af
Weronika <w.t.wegier@gmail.com>
2020-05-26 09:44:15 +0200

    Added experiment script, AUE extension and HT wrapper

--- AWE.py
+++ AWE.py
@@ -3,20 +3,30 @@ from sklearn.ensemble import BaseEnsemble
 from sklearn.utils.validation import check_X_y, check_array, check_is_fitted
 from sklearn.utils.multiclass import _check_partial_fit_first_call
 from sklearn.model_selection import StratifiedShuffleSplit
+from sklearn.metrics import f1_score,  balanced_accuracy_score as bac
+from imblearn.metrics import geometric_mean_score as g_mean
+from imblearn.over_sampling import RandomOverSampler
+from imblearn.under_sampling import RandomUnderSampler
 import numpy as np
+import math
 
 WEIGHT_CALCULATION = (
    "proportional_to_mse",
+    "proportional_to_f1",
+    "proportional_to_g-mean",
+    "proportional_to_bac"
 )
 
 
 class AWE(BaseEnsemble, ClassifierMixin):
 
-    def __init__(self, base_estimator=None, n_estimators=10, weighting_method="proportional_to_mse"):
+    def __init__(self, base_estimator=None, n_estimators=10, weighting_method="proportional_to_mse", sampling=None, update=False):
 
         self.base_estimator = base_estimator
         self.n_estimators = n_estimators
         self._weighting_method = weighting_method
+        self._sampling = sampling
+        self._update = update
         self.ensemble_ = None
         self.weights_ = None
         self.classes_ = None
@@ -38,18 +48,64 @@ class AWE(BaseEnsemble, ClassifierMixin):
     def _random_mean_squared_error(y):
         return sum([sum(y == u) / len(y) * (1 - (sum(y == u) / len(y)))**2 for u in np.unique(y)])
 
-    def _get_weigth_for_candidate(self, candidate_clf):
+    def _weight_of_random_classifier(self, y):
         if self._weighting_method == "proportional_to_mse":
-            sss = StratifiedShuffleSplit(n_splits=5, test_size=0.5)
-            mse = []
-            for train_index, test_index in sss.split(self.X_, self.y_):
-                candidate_clf.fit(self.X_[train_index], self.y_[train_index])
-                mse.append(self._random_mean_squared_error(self.y_[test_index]) - self._mean_squared_error(self.y_[test_index], candidate_clf.predict_proba(self.X_[test_index])))
-                candidate_clf.fit(self.X_[test_index], self.y_[test_index])
-                mse.append(self._random_mean_squared_error(self.y_[train_index]) - self._mean_squared_error(self.y_[train_index], candidate_clf.predict_proba(self.X_[train_index])))
-            return sum(mse) / len(mse)
+            return 1 / self._random_mean_squared_error(y)
+        P = sum(y == 1)
+        N = sum(y == 0)
+        pP = P / len(y)
+        pN = N / len(y)
+        TP = P*pP
+        TN = N*pN
+        FP = N*pP
+        FN = P*pN
+        if self._weighting_method == "proportional_to_f1":
+            return 2*TP / (2*TP + FP + FN)
         else:
-            raise NotImplementedError
+            sens = TP / (TP + FP)
+            spec = TN / (TN + FP)
+            if self._weighting_method == "proportional_to_g-mean":
+                return math.sqrt(sens * spec)
+            elif self._weighting_method == "proportional_to_bac":
+                return (sens + spec) / 2
+            else:
+                raise NotImplementedError
+
+
+    def _get_weigth_for_candidate(self, candidate_clf):
+
+        sss = StratifiedShuffleSplit(n_splits=5, test_size=0.5)
+        weight = []
+        for train_index, test_index in sss.split(self.X_, self.y_):
+            for i in range(2):
+                if self._sampling == 'over':
+                    ros = RandomOverSampler(random_state=0)
+                    X, y = ros.fit_resample(self.X_[train_index], self.y_[train_index])
+                elif self._sampling == 'under':
+                    rus = RandomUnderSampler(random_state=0)
+                    X, y = rus.fit_resample(self.X_[train_index], self.y_[train_index])
+                else:
+                    X, y = self.X_[train_index], self.y_[train_index]
+                candidate_clf.fit(X, y)
+                if self._weighting_method == "proportional_to_mse" and not self._update:
+                    weight.append(self._random_mean_squared_error(self.y_[test_index]) - self._mean_squared_error(
+                        self.y_[test_index], candidate_clf.predict_proba(self.X_[test_index])))
+                elif self._weighting_method == "proportional_to_mse" and self._update:
+                    weight.append(1 / (self._mean_squared_error(self.y_[test_index], candidate_clf.predict_proba(self.X_[test_index])) + 0.001))
+                elif self._weighting_method == "proportional_to_f1":
+                    weight.append(f1_score(self.y_[test_index], candidate_clf.predict(self.X_[test_index])))
+                elif self._weighting_method == "proportional_to_g-mean":
+                    weight.append(g_mean(self.y_[test_index], candidate_clf.predict(self.X_[test_index])))
+                elif self._weighting_method == "proportional_to_bac":
+                    weight.append(bac(self.y_[test_index], candidate_clf.predict(self.X_[test_index])))
+                else:
+                    raise NotImplementedError
+
+                train_index, test_index = test_index, train_index
+
+            return sum(weight) / len(weight)
+
+
 
     def fit(self, X, y):
         """Fitting."""
@@ -70,6 +126,7 @@ class AWE(BaseEnsemble, ClassifierMixin):
         self.X_ = X
         self.y_ = y
 
+        # print(X[0,0])
         if _check_partial_fit_first_call(self, classes):
             self.classes_ = classes
 
@@ -88,9 +145,24 @@ class AWE(BaseEnsemble, ClassifierMixin):
 
         candidate_clf = clone(self.base_estimator)
         candidate_weight = self._get_weigth_for_candidate(candidate_clf)
-        candidate_clf.fit(X, y)
+        if self._sampling == 'over':
+            ros = RandomOverSampler(random_state=0)
+            X, y = ros.fit_resample(X, y)
+        elif self._sampling == 'under':
+            rus = RandomUnderSampler(random_state=0)
+            X, y = rus.fit_resample(X, y)
+        if not self._update:
+            candidate_clf.fit(X, y)
+        else:
+            candidate_clf.partial_fit(X, y)
 
         self._set_weights()
+
+        if self._update:
+            random_cl_weight = self._weight_of_random_classifier(y)
+            for i in range(len(self.ensemble_)):
+                if self.weights_[i] > random_cl_weight:
+                    self.ensemble_[i].partial_fit(X,y)
 
         self.ensemble_.append(candidate_clf)
 
@@ -106,15 +178,36 @@ class AWE(BaseEnsemble, ClassifierMixin):
     def _set_weights(self):
 
         """Wang's weights"""
-        if self._weighting_method == "proportional_to_mse":
+        if self._weighting_method == "proportional_to_mse" and not self._update:
             mse_rand = self._random_mean_squared_error(self.y_)
-            mse_members = np.array([self._mean_squared_error(self.y_, member_clf.predict_proba(self.X_), )
+            mse_members = np.array([self._mean_squared_error(self.y_, self.check_proba_array(member_clf.predict_proba(self.X_)))
                                     for member_clf in self.ensemble_])
             self.weights_ = mse_rand - mse_members
+        elif self._weighting_method == "proportional_to_mse" and self._update:
+            self.weights_ = np.array([1 / (self._mean_squared_error(self.y_, self.check_proba_array(member_clf.predict_proba(self.X_))) + 0.001)
+                                    for member_clf in self.ensemble_])
+        elif self._weighting_method == "proportional_to_f1":
+            self.weights_ = np.array([f1_score(self.y_, member_clf.predict(self.X_)) for member_clf in self.ensemble_])
+        elif self._weighting_method == "proportional_to_g-mean":
+            self.weights_ = np.array([g_mean(self.y_, member_clf.predict(self.X_)) for member_clf in self.ensemble_])
+        elif self._weighting_method == "proportional_to_bac":
+            self.weights_ = np.array([bac(self.y_, member_clf.predict(self.X_)) for member_clf in self.ensemble_])
+        else:
+            raise NotImplementedError
 
     def score(self, X, y, sample_weight=None):
         prediction = self.predict(X)
         return sum(prediction == y) / len(y)
+
+    # def check_proba_array(self, proba):
+    #     if len(proba.shape) == 2:
+    #         return proba
+    #     else:
+    #         for i in range(proba.shape[0]):
+    #             if proba[i].shape[0] == 1:
+    #                 proba[i] = np.append(proba[i],[0.0])
+    #         proba = np.stack(proba)
+    #         return proba
 
     def ensemble_support_matrix(self, X):
         """ESM."""
@@ -148,4 +241,10 @@ class AWE(BaseEnsemble, ClassifierMixin):
         prediction = np.argmax(supports, axis=1)
 
         return self.classes_[prediction]
+
+    def get_name(self):
+        if self._update:
+            return "AUE-" + self._weighting_method + '-' + self._sampling
+        else:
+            return "AWE-" + self._weighting_method + '-' + self._sampling
 
--- Experiment.py
+++ Experiment.py
@@ -1,16 +1,106 @@
 from AWE import AWE
 from sklearn.naive_bayes import GaussianNB
+from Classifier_wrapper import HoeffdingTreeWrapper
 from strlearn.streams import StreamGenerator
+from strlearn.ensembles import UOB, OOB, WAE
 from sklearn.metrics import accuracy_score
-from strlearn.utils.metrics import bac
+from sklearn.metrics import f1_score,  balanced_accuracy_score as bac
+from imblearn.metrics import geometric_mean_score as g_mean
 
 from strlearn.evaluators import TestThenTrain
 
-clf = GaussianNB()
+import matplotlib.pyplot as plt
+import numpy as np
 
-ensemble = AWE(clf)
-stream = StreamGenerator(n_chunks=30, n_drifts=1)
-metrics = [accuracy_score, bac]
-evaluator = TestThenTrain(metrics)
-evaluator.process(stream, ensemble)
-print(evaluator.scores)
+
+class Experiment:
+
+    def __init__(self, streams_random_seeds=None, ensembles=(), ensembles_labels=(), metrics=None,
+                 imbalance=None, gradual=True, n_chunks=100):
+        if metrics is None:
+            metrics = [accuracy_score]
+        if imbalance is None:
+            imbalance = [0.50, 0.50]
+        self._streams_random_seeds = streams_random_seeds
+        self._ensembles = ensembles
+        self._metrics = metrics
+        self._ensembles_labels = ensembles_labels
+        self._proportions = imbalance
+        self._gradual_drift = gradual
+        self.n_chunks = n_chunks
+        self._evaluator = TestThenTrain(self._metrics)
+        self._scores = np.empty((len(self._streams_random_seeds), len(self._ensembles), self.n_chunks-1, len(self._metrics)))
+
+    def conduct(self, file=None):
+        for r_i, r in enumerate(self._streams_random_seeds):
+
+            if self._gradual_drift:
+                stream = StreamGenerator(n_chunks=self.n_chunks, chunk_size=500, n_drifts=5, weights=self._proportions,
+                                         n_features=2,
+                                         n_informative=2, n_redundant=0, n_repeated=0, random_state=r, concept_sigmoid_spacing=5)
+            else:
+                stream = StreamGenerator(n_chunks=self.n_chunks, chunk_size=500, n_drifts=5, weights=self._proportions, n_features=2,
+                                     n_informative=2, n_redundant=0, n_repeated=0, random_state=r)
+
+            self._evaluator.process(stream, self._ensembles)
+
+            self._scores[r_i, :, :, :] = self._evaluator.scores[:, :, :]
+
+        if file is not None:
+            np.save(file, self._scores)
+
+
+    def make_plot(self, title):
+        results = np.mean(self._scores, axis=0)
+        metrics_labels = [metric.__name__ for metric in self._metrics]
+        for i in range(len(self._metrics)):
+            for j in range(len(self._ensembles)):
+                plt.plot(results[j, :, i], label=self._ensembles_labels[j])
+
+            plt.title(metrics_labels[i])
+            plt.ylim(0, 1)
+            plt.ylabel('Jakość')
+            plt.xlabel('Chunk')
+
+            plt.legend()
+            plt.savefig(title + '_' + metrics_labels[i] + ".png")
+            plt.clf()
+
+
+
+if __name__ == '__main__':
+    clf = HoeffdingTreeWrapper()
+
+    random_seeds = [4, 13, 42, 44, 666]
+
+    imbalance = [0.05, 0.10, 0.20, 0.30]
+
+    ensemble1 = AWE(clf, 10, "proportional_to_g-mean", sampling="under")
+    ensemble2 = AWE(clf, 10, "proportional_to_bac", sampling="under")
+    ensemble3 = AWE(clf, 10, "proportional_to_f1", sampling="under")
+    ensemble4 = AWE(clf, 10, "proportional_to_g-mean", sampling="over")
+    ensemble5 = AWE(clf, 10, "proportional_to_bac", sampling="over")
+    ensemble6 = AWE(clf, 10, "proportional_to_f1", sampling="over")
+    ensemble7 = AWE(clf, 10, "proportional_to_g-mean", sampling="under", update=True)
+    ensemble8 = AWE(clf, 10, "proportional_to_bac", sampling="under", update=True)
+    ensemble9 = AWE(clf, 10, "proportional_to_f1", sampling="under", update=True)
+    ensemble10 = AWE(clf, 10, "proportional_to_g-mean", sampling="over", update=True)
+    ensemble11 = AWE(clf, 10, "proportional_to_bac", sampling="over", update=True)
+    ensemble12 = AWE(clf, 10, "proportional_to_f1", sampling="over", update=True)
+    ensemble13 = WAE(clf, 10)
+    ensemble14 = OOB(clf, 10)
+    ensemble15 = UOB(clf, 10)
+    ensembles = (ensemble1, ensemble2, ensemble3, ensemble4, ensemble5, ensemble6, ensemble7, ensemble8, ensemble9,
+                 ensemble10, ensemble11, ensemble12, ensemble13, ensemble14, ensemble15)
+    ensemble_labels = [ensembles[i].get_name() for i in range(12)] + ['WAE', 'OOB', 'UOB']
+
+    path = 'wyniki/'
+    for i in imbalance:
+        e = Experiment(streams_random_seeds=random_seeds, ensembles=ensembles, ensembles_labels=ensemble_labels,
+                       metrics=[f1_score, g_mean, bac], imbalance=[1 - i, i], gradual=False)
+        e.conduct(file=path+'Stream_sudden_drift_'+str(int(100*i))+'_imbalance.npy')
+
+        e = Experiment(streams_random_seeds=random_seeds, ensembles=ensembles, ensembles_labels=ensemble_labels,
+                       metrics=[f1_score, g_mean, bac], imbalance=[1 - i, i], gradual=True)
+        e.conduct(file=path+'Stream_gradual_drift_'+str(int(100*i))+'_imbalance.npy')
+
